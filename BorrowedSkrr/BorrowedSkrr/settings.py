efdb549df15bb8e1079c9a2070cc61dd83d591c6
이현승 <77336664+gesal03@users.noreply.github.com>
2023-08-18 21:04:46 +0900

    ㅁㅇㅁㄴㅇ

--- BorrowedSkrr/BorrowedSkrr/settings.py
+++ BorrowedSkrr/BorrowedSkrr/settings.py
@@ -37,10 +37,10 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware', # <- 가능한 높게 위치시켜야 한다.
-    # 'django.middleware.security.SecurityMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
